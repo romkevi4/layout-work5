16ae2496633dd4e4815fef9fc0e6c7512a64837d
Roman Shatskikh <shatskikh.roman@gmail.com>
2022-12-27 01:30:44 +0300

    feat: add pop-up and its opening-closing

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+const popup = document.querySelector('#popup');
+const createNewShop = document.querySelector('#createNewShop');
+const closePopup = document.querySelector('#closePopup');
+
+createNewShop.addEventListener('click', () => {
+    popup.classList.remove('d-none');
+    popup.classList.add('d-flex');
+});
+
+closePopup.addEventListener('click', () => {
+    popup.classList.remove('d-flex');
+    popup.classList.add('d-none');
+});
